bb091610ff3e78a92b97e51b0f009f98b2cc491d
Durgesh Singh <61835021+durgeshsingh-17@users.noreply.github.com>
2021-09-15 18:36:23 +0530

    Add files via upload

---
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -0,0 +1,19 @@
+function almostIncreasingSequence(sequence) {
+    var isFirst = true;
+    for (var i = 0; i < sequence.length; i++) {
+        if (sequence[i] >= sequence[i + 1]) {
+            if (isFirst) {
+                isFirst = false;
+                if (
+                    sequence[i - 1] >= sequence[i + 1] &&
+                    sequence[i] >= sequence[i + 2]
+                ) {
+                    return false;
+                }
+            } else return false;
+        }
+    }
+    return true;
+}
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
