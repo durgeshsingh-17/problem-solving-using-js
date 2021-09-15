1216a0054addb1dbf2f651914e85d190673f8968
Durgesh Singh <61835021+durgeshsingh-17@users.noreply.github.com>
2021-09-15 18:36:54 +0530

    Add files via upload

---
+++ areEquallyStrong/areEquallyStrong.js
@@ -0,0 +1,12 @@
+// const areEquallyStrong = (yl, yr, fl, fr) => yl + yr === fl + fr;
+// console.log(areEquallyStrong(10,15,15,10));
+// console.log(areEquallyStrong(10,5,15,9));
+
+
+
+function areEquallyStrong(yl, yr, fl, fr) {
+	return yl + yr === fl + fr;
+}
+
+console.log(areEquallyStrong(10,15,15,10));
+console.log(areEquallyStrong(10,5,15,9));
